5f05757a2de0b4bf3d4551c96eaf3a4d9994802f
sksazid01 <107117503+sksazid01@users.noreply.github.com>
2025-03-23 15:02:14 +0600

    Update Remove All blank row.js

--- Google Sheet Code/Remove All blank row.js
+++ Google Sheet Code/Remove All blank row.js
@@ -1,1 +1,12 @@
 
+function removeBlankRowsInColumnB() {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var data = sheet.getDataRange().getValues();
+  var numRows = data.length;
+
+  for (var i = numRows - 1; i >= 0; i--) {
+    if (data[i][1] === "") { // Check if column B (index 1) is empty
+      sheet.deleteRow(i + 1); // Delete the row (i + 1 because rows are 1-indexed)
+    }
+  }
+}
